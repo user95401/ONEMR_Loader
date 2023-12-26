4f102245aaa94d7e3c93d13c1ca752f4d133413a
user95401 <user95401@gmail.com>
2023-12-26 15:32:41 +0400

    1.3.0
    info
    menulayer
    info in configure file

--- src/_main.cpp
+++ src/_main.cpp
@@ -38,19 +38,24 @@ DWORD WINAPI LoadMods(void* hModule) {
 
 //0x272390
 void __fastcall LoadingLayer_loadAssets(CCLayer* self, void* edx) {
+    afterLoad = true;
     MappedHooks::getOriginal(LoadingLayer_loadAssets)(self, edx);
     if (!CCFileUtils::sharedFileUtils()->isFileExist("ONEMR_Loader.NoInfo") && !CCFileUtils::sharedFileUtils()->isFileExist("ONEMR_Loader.AddInfo")) {
+        std::string fileinfo = (
+            "u can delete that file, so message box with confirm appears again" "\n"
+            "if exists ONEMR_Loader.NoInfo => dont show loaded mods info in corner" "\n"
+            "if exists ONEMR_Loader.AddInfo => dont show confirm message box"
+            );
         if (MessageBoxExA(nullptr, "Enable showing info?", "ONEMR_Loader", MB_ICONQUESTION | MB_YESNO, LANG_ENGLISH) == IDYES) {
             std::remove("ONEMR_Loader.NoInfo");
-            std::ofstream("ONEMR_Loader.AddInfo");
+            std::ofstream("ONEMR_Loader.AddInfo") << fileinfo;
         }
         else {
-            std::ofstream("ONEMR_Loader.NoInfo");
+            std::ofstream("ONEMR_Loader.NoInfo") << fileinfo;
             std::remove("ONEMR_Loader.AddInfo");
         }
     }
     if (CCFileUtils::sharedFileUtils()->isFileExist("ONEMR_Loader.NoInfo")) return self->removeChildByTag(938);
-    afterLoad = true;
     self->removeChildByTag(938);
     CCLabelTTF* ModsCountLabel = CCLabelTTF::create(std::format(
         "ONEMR_Loader: {} dlls loaded{}", 
@@ -62,31 +67,41 @@ void __fastcall LoadingLayer_loadAssets(CCLayer* self, void* edx) {
     ModsCountLabel->setScale(0.4f);
     ModsCountLabel->setOpacity(68);
     ModsCountLabel->setBlendFunc({ GL_SRC_ALPHA, GL_ONE }/*that is additive blend*/);
-    ModsCountLabel->runAction(CCFadeTo::create(0.1f, 32));//wow gd 2.2 have do smth with opacity stuff
     self->addChild(ModsCountLabel, 10, 938);/**/
 }
 
-//590792
-//bool __fastcall MenuLayer_init(CCLayer* self, void* edx) {
-//    CCMessageBox(__FUNCTION__, __FUNCTION__);
-//    MappedHooks::getOriginal(MenuLayer_init)(self, edx);
-//    self->setRotation(12);
-//    twoTimesBoolCallEscapeByParrentNode(self);
-//    if (!afterLoad) return true;
-//    else afterLoad = false;
-//    CCLabelTTF* ModsCountLabel = CCLabelTTF::create(std::format("ONEMR_Loader: {} dlls loaded", ModsLoaded).c_str(), "Arial", 6.000f);
-//    ModsCountLabel->setAnchorPoint(CCPointZero);
-//    //ModsCountLabel->setOpacity(32);
-//    ModsCountLabel->runAction(CCFadeTo::create(5.0f, 0));
-//    self->addChild(ModsCountLabel, 10, 938);
-//    return true;
-//}
+//0x276700
+bool __fastcall MenuLayer_init(CCLayer* self, void* edx) {
+    MappedHooks::getOriginal(MenuLayer_init)(self, edx);
+    //afterLoad
+    if (!afterLoad) return true;
+    afterLoad = false;
+    //ONEMR_Loader.NoInfo
+    if (CCFileUtils::sharedFileUtils()->isFileExist("ONEMR_Loader.NoInfo")) return true;
+    //geode thook origin call bug
+    twoTimesBoolCallEscapeByParrentNode(self);
+    //add label
+    CCLabelTTF* ModsCountLabel = CCLabelTTF::create(std::format(
+        "ONEMR_Loader: {} dlls loaded{}",
+        ModsLoaded,
+        ModsLoadedList == "" ? "" : ("\n" + ModsLoadedList)
+    ).c_str(), "Arial", 12.000f);
+    ModsCountLabel->setHorizontalAlignment(CCTextAlignment::kCCTextAlignmentLeft);
+    ModsCountLabel->setAnchorPoint({ -0.01f, -0.1f });
+    ModsCountLabel->setScale(0.4f);
+    ModsCountLabel->setOpacity(68);
+    ModsCountLabel->setBlendFunc({ GL_SRC_ALPHA, GL_ONE }/*that is additive blend*/);
+    ModsCountLabel->runAction(CCEaseExponentialIn::create(CCFadeTo::create(3.0f, 0)));
+    self->addChild(ModsCountLabel, 10, 938);
+    return true;
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         LoadMods(hModule);
         MH_Initialize();
         MappedHooks::registerHook(base + 2565008, LoadingLayer_loadAssets);
+        MappedHooks::registerHook(base + 0x276700, MenuLayer_init);
     }
     return TRUE;
 }
